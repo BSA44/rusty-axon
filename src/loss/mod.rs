bc8547b420fd77bc8be25bf30ec581756d99aa1c
sarvarC7 <sarvar@c7-security.com>
2025-11-29 18:39:24 +0500

    added rmse

--- src/loss/mod.rs
+++ src/loss/mod.rs
@@ -1,7 +1,8 @@
 pub mod loss;
 pub mod mse;
 pub mod cross_entropy;
-
+pub mod rmse;
 pub use loss::Loss;
 pub use mse::MeanSquaredError;
 pub use cross_entropy::CrossEntropy;
+pub use rmse::RootMeanSquaredError;
---
+++ src/loss/rmse.rs
@@ -0,0 +1,16 @@
+use crate::engine::value::Node;
+use crate::loss::loss::Loss;
+
+
+pub struct RootMeanSquaredError;
+
+impl Loss for RootMeanSquaredError {
+    fn forward(&self, predictions: &[Node], targets: &[Node]) -> Node {
+        assert_eq!(predictions.len(), targets.len(), "Predictions and targets must have the same length");
+        let mut loss = Node::new(0.0);
+        for (prediction, target) in predictions.iter().zip(targets.iter()) {
+            loss = loss + (prediction.clone() - target.clone()).pow(2.0);
+        }
+        (loss / predictions.len() as f64).pow(0.5)
+    }
+}
